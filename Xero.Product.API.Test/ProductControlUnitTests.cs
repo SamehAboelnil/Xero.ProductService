6bf937b3d457e9ca3ffd0a35c42a869a6a6f8b2b
SamehAboelnil <sameh.aboelnil@gmail.com>
2019-12-02 21:02:46 +1100

    Add more tests, use fluent Assertion package

--- Xero.Product.API.Test/ProductControlUnitTests.cs
+++ Xero.Product.API.Test/ProductControlUnitTests.cs
@@ -11,28 +11,33 @@ using Xero.Product.API;
 using Xero.Product.API;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using FluentAssertions;
 
 namespace Xero.Product.API.UnitTests
 {
     [TestClass]
     public class ProductControlUnitTests
     {
-        [TestMethod]
-        public async Task Get_Return_Products_OK_Wehn_DataRepo_Have_Products()
-        {
-            var result = new List<ProductData>();
-            result.Add(new ProductData { DeliveryPrice = 2, Description = "Test", Id = Guid.NewGuid(), Name = " TestName", Price = 123 });
-            var mockProductRepository = new Mock<IProductRepository>();
-            mockProductRepository
-                .Setup(repo => repo.GetAllProducts(It.IsAny<string>()))
-                .Returns(Task.FromResult(result.AsEnumerable()));
+        IMapper Mapper;
+        Guid newGuid = new Guid("d6c21ad4-8e98-40ed-aa13-b8807051dee6");
+        public Guid NewGuid { get => newGuid; set => newGuid = value; }
 
+
+        readonly API.Models.Products productsResponse = new API.Models.Products(new List<Models.ProductData>()
+        {
+            new API.Models.ProductData { DeliveryPrice = 2, Description = "Test", Id = new Guid(), Name = " TestName", Price = 123 }
+        });
+
+        readonly ProductData noResult = new ProductData() { };
+
+        [TestInitialize]
+        public void Initalize()
+        {
 
             Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
             {
                 MapperConfiguration config = new MapperConfiguration(cfg =>
                 {
-                    // This line ensures that internal properties are also mapped over.
                     cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
                     cfg.AddProfile<ModelsProfile>();
                 });
@@ -40,14 +45,63 @@ namespace Xero.Product.API.UnitTests
                 return mapper;
             });
 
-            IMapper Mapper = Lazy.Value;
+            Mapper = Lazy.Value;
+        }
+        [TestMethod]
+        public async Task Get_Return_Products_OK_Wehn_DataRepo_Have_Products()
+        {
+            List<ProductData> resultWithOneRecord = new List<ProductData>
+            {
+                new ProductData { DeliveryPrice = 2, Description = "Test", Id = NewGuid, Name = " TestName", Price = 123 }
+            };
 
-            Xero.Product.API.Controllers.ProductsController productsController = new API.Controllers.ProductsController(mockProductRepository.Object, Mapper);
-            var result1 = await productsController.Get("TestName");
+            var mockProductRepository = new Mock<IProductRepository>();
+            mockProductRepository
+                .Setup(repo => repo.GetAllProducts(It.IsAny<string>()))
+                .Returns(Task.FromResult(resultWithOneRecord.AsEnumerable()));
 
-            
+            var result1 = await new Controllers.ProductsController(mockProductRepository.Object, Mapper).Get("TestName");
             var statusCode = result1.Result;
             Assert.IsInstanceOfType(statusCode, typeof(OkObjectResult));
+
+        }
+
+        [TestMethod]
+        public async Task Get_BY_ID_Return_Not_Found_Wehn_DataRepo_Have_No_Products()
+        {
+            var mockProductRepository = new Mock<IProductRepository>();
+            mockProductRepository
+                .Setup(repo => repo.GetProduct(It.IsAny<Guid>()))
+                .Throws(new ProductNotFoundException());
+
+            var result1 = await new Controllers.ProductsController(mockProductRepository.Object, Mapper).Get(NewGuid);
+            var statusCode = result1.Result;
+            Assert.IsInstanceOfType(statusCode, typeof(NotFoundObjectResult));
+
+        }
+
+        [TestMethod]
+        public async Task Get_BY_ID_Return_Product_OK_Wehn_DataRepo_Have_No_Products()
+        {
+            Models.ProductData productResponse = new API.Models.ProductData { DeliveryPrice = 2, Description = "Test", Id = NewGuid, Name = " TestName", Price = 123 };
+            ProductData resultOneProduct = new ProductData { DeliveryPrice = 2, Description = "Test", Id = NewGuid, Name = " TestName", Price = 123 };
+
+
+            var mockProductRepository = new Mock<IProductRepository>();
+            mockProductRepository
+                .Setup(repo => repo.GetProduct(It.IsAny<Guid>()))
+                .Returns(Task.FromResult(resultOneProduct));
+
+            var result1 = await new Controllers.ProductsController(mockProductRepository.Object, Mapper).Get(NewGuid);
+            var statusCode = result1.Result;
+            Assert.IsInstanceOfType(statusCode, typeof(OkObjectResult));
+
+            var result = statusCode as OkObjectResult;
+            var response = result.Value;
+            response.Should().BeEquivalentTo(productResponse);
+
+
+
 
         }
     }
