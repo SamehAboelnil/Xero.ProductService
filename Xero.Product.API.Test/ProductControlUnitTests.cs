4c7ff63a9076dbf3cdaf95f2b653dc99d79e3d44
SamehAboelnil <sameh.aboelnil@gmail.com>
2019-12-02 21:15:31 +1100

    Add more tests and I'm tired

--- Xero.Product.API.Test/ProductControlUnitTests.cs
+++ Xero.Product.API.Test/ProductControlUnitTests.cs
@@ -3,14 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
 using Xero.Product.Data;
-using Xero.Product.API;
-using Xero.Product.API;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 using FluentAssertions;
 
 namespace Xero.Product.API.UnitTests
@@ -21,12 +17,6 @@ namespace Xero.Product.API.UnitTests
         IMapper Mapper;
         Guid newGuid = new Guid("d6c21ad4-8e98-40ed-aa13-b8807051dee6");
         public Guid NewGuid { get => newGuid; set => newGuid = value; }
-
-
-        readonly API.Models.Products productsResponse = new API.Models.Products(new List<Models.ProductData>()
-        {
-            new API.Models.ProductData { DeliveryPrice = 2, Description = "Test", Id = new Guid(), Name = " TestName", Price = 123 }
-        });
 
         readonly ProductData noResult = new ProductData() { };
 
@@ -48,26 +38,35 @@ namespace Xero.Product.API.UnitTests
             Mapper = Lazy.Value;
         }
         [TestMethod]
-        public async Task Get_Return_Products_OK_Wehn_DataRepo_Have_Products()
+        public async Task Get_Return_Products_OK_When_DataRepo_Have_Products()
         {
             List<ProductData> resultWithOneRecord = new List<ProductData>
             {
                 new ProductData { DeliveryPrice = 2, Description = "Test", Id = NewGuid, Name = " TestName", Price = 123 }
             };
 
+            Models.Products productsResponse = new API.Models.Products(new List<Models.ProductData>()
+            {
+                new API.Models.ProductData { DeliveryPrice = 2, Description = "Test", Id = newGuid, Name = " TestName", Price = 123 }
+            });
+
             var mockProductRepository = new Mock<IProductRepository>();
             mockProductRepository
                 .Setup(repo => repo.GetAllProducts(It.IsAny<string>()))
                 .Returns(Task.FromResult(resultWithOneRecord.AsEnumerable()));
 
-            var result1 = await new Controllers.ProductsController(mockProductRepository.Object, Mapper).Get("TestName");
-            var statusCode = result1.Result;
+            var statusCode = (await new Controllers.ProductsController(mockProductRepository.Object, Mapper).Get("TestName")).Result;
             Assert.IsInstanceOfType(statusCode, typeof(OkObjectResult));
+
+            var result = statusCode as OkObjectResult;
+            var response = result.Value;
+            response.Should().BeEquivalentTo(productsResponse);
+
 
         }
 
         [TestMethod]
-        public async Task Get_BY_ID_Return_Not_Found_Wehn_DataRepo_Have_No_Products()
+        public async Task Get_BY_ID_Return_Not_Found_When_DataRepo_Have_No_Products()
         {
             var mockProductRepository = new Mock<IProductRepository>();
             mockProductRepository
